8df8ab03925520f5562c8924c80c9ed31f9f95d0
Gabriel Wortmann <wortmanngabriel@gmail.com>
2023-09-23 14:44:42 -0500

    finishes DOM lab #2

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const body = document.getElementsByTagName("body")[0];
+
+body.innerHTML = '<h1 id="victory">Gabe is the champion</h1><script type="text/javascript" src="index.js"></script>'
+
+const newHeader = document.getElementsByTagName("h1")[0];
